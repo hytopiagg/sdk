f5c5692a1589e43a56f0483f4caf60e1cb46f5c9
ArkDev <me@braydonb.com>
2025-11-17 12:14:03 -0800

    Fixed webtransport bundling.

--- bin/scripts.js
+++ bin/scripts.js
@@ -466,7 +466,7 @@ async function packageProject() {
 async function build(devMode = false) {
   let envFlags = devMode ? '' : '--minify --sourcemap=inline';
 
-  execSync(`npx --yes bun build --target=node --env=disable --format=esm ${envFlags} --external=@fails-components/webtransport-transport-http3-quiche --outfile=index.mjs index.ts`, { stdio: 'inherit' });
+  execSync(`npx --yes bun build --target=node --env=disable --format=esm ${envFlags} --external=@fails-components/webtransport --external=@fails-components/webtransport-transport-http3-quiche --outfile=index.mjs index.ts`, { stdio: 'inherit' });
 }
 
 /**
